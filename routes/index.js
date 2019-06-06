e4b5304f796f6800736fbf6440ca201c20725a3a
Setu Potnis <setupotnis@gmail.com>
2019-06-06 17:03:54 -0400

    added html form

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ var router = express.Router();
 let landing = require('../controllers/landing');
 /* GET home page. */
 router.get('/', landing.get_landing);
+router.post('/', landing.submit_lead);
 
 module.exports = router;
