e02b06c473df24046f6651263cad41f09f92e9e8
Setu Potnis <setupotnis@gmail.com>
2019-06-06 15:57:10 -0400

    created html form

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-let index = require('../controllers/index');
+let landing = require('../controllers/landing');
 /* GET home page. */
-router.get('/', index.index);
+router.get('/', landing.get_landing);
 
 module.exports = router;
